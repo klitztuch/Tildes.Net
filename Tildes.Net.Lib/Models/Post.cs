7ca19fc8a2012ad2da2fe4b64112ff981317214f
Markus Rothlübbers <markus@mrlb.space>
2023-06-16 00:09:15 +0200

    Add Text property to post

--- Tildes.Net.Lib/Models/Post.cs
+++ Tildes.Net.Lib/Models/Post.cs
@@ -5,6 +5,7 @@ public class Post
     public string? Id { get; set; }
     public string? Title { get; set; }
     public PostMetadata? Metadata { get; set; }
+    public string[]? Text { get; set; }
     public string? TextExcerpt { get; set; }
     public PostInfo? Info { get; set; }
 }
